d4c7de4f218fa38c6303edb13f5ce966f2961668
Blokha Yaroslav <ipz213_byas@student.ztu.edu.ua>
2022-11-06 23:16:56 +0200

    Replaced text in the branch text

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Текст 1");
-Console.WriteLine("Текст 2");
-Console.WriteLine("Текст 3");
+Console.WriteLine("Замінений текст 1");
+Console.WriteLine("Замінений текст 2");
+Console.WriteLine("Замінений текст 3");
 Console.WriteLine("Текст 4");
 Console.WriteLine("Текст 5");
