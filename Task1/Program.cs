e4a1cd0806b96a0fd900a740bb1eb3e7ce2b969a
Blokha Yaroslav <ipz213_byas@student.ztu.edu.ua>
2022-11-06 23:08:32 +0200

    Add 5 texts

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Текст 1");
+Console.WriteLine("Текст 2");
+Console.WriteLine("Текст 3");
+Console.WriteLine("Текст 4");
+Console.WriteLine("Текст 5");
